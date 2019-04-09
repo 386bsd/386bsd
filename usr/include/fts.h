e2ac0c65ccbdeffc5731573298d1bdff4aefd285
William Jolitz <william.jolitz@gmail.com>
2019-04-09 09:55:56 -1000

    remove dregs and rcs comment

--- usr/include/fts.h
+++ usr/include/fts.h
@@ -30,7 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)fts.h	8.1 (Berkeley) 6/2/93
+ * $Id: fts.h,v 1.1 94/10/20 00:53:22 bill Exp $
  */
 
 #ifndef	_FTS_H_
